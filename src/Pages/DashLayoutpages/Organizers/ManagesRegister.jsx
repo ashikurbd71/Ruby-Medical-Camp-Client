e5154e654ac443bb0058151c3f897e411f649173
ashikurbd71 <ashikurovi2003@gmail.com>
2023-11-26 04:05:21 +0600

    avalible section cad show

--- src/Pages/DashLayoutpages/Organizers/ManagesRegister.jsx
+++ src/Pages/DashLayoutpages/Organizers/ManagesRegister.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import SectionTitle from '../../../Shared/SectionTitle';
 import { Helmet } from 'react-helmet';
+import Container from '../../../Shared/Container';
+import { FaEdit } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const ManagesRegister = () => {
     return (
@@ -11,8 +14,165 @@ const ManagesRegister = () => {
         
     </Helmet>
         <div>
-            <SectionTitle heading={'Manages Register'}/>
+            <SectionTitle heading={'Manages Register'} title={'All Register User'}/>
         </div>
+
+        <Container>
+        <div className="rounded-lg border border-gray-200">
+  <div className="overflow-x-auto rounded-t-lg">
+    <table className="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
+      <thead className="ltr:text-left rtl:text-right bg-[#1976D2]">
+        <tr>
+          <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+            Camp Name
+          </th>
+          <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+            Service
+          </th>
+          <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+          Healthcare
+          </th>
+          <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+            Location
+          </th>
+          <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+            Date
+          </th>
+          <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+            Fee
+          </th>
+          <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+            Update
+          </th>
+          <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+            Cancel
+          </th>
+        </tr>
+      </thead>
+
+      <tbody className="divide-y divide-[#1976D2]">
+        
+        {/* {
+            campdata?.map(data => 
+                
+                <tr key={data?._id}>
+                <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                  {data?.campname}
+                </td>
+                <td className="whitespace-nowrap px-4 py-2 text-gray-900"> {data?.services}</td>
+                <td className="whitespace-nowrap px-4 py-2 text-gray-900">
+                {data?.professional}
+                </td>
+                <td className="whitespace-nowrap px-4 py-2 t text-gray-900">
+              {data?.location}
+                    </td>
+                    <td className="whitespace-nowrap px-4 py-2 t text-gray-900">
+              {data?.date}
+                    </td>
+                    <td className="whitespace-nowrap px-4 py-2 t text-gray-900">
+             ${data?.fees}
+                    </td>
+                    <td  className="whitespace-nowrap px-4 py-2 t text-gray-900">
+           
+             <Link to={`/dashboard/update-camp/${data?._id}`}>
+             <FaEdit className='text-2xl text-green-500'/>
+             </Link>
+                    </td>
+
+                    
+                    <td className="whitespace-nowrap px-4 py-2 t text-gray-900">
+                   {/* <button onClick={() => handledelte(data?._id)}> <FaTrashAlt className='text-2xl text-red-500'/></button> */}
+                    {/* </td>
+              </tr>    */}
+                
+                {/* ) */}
+        {/* */} 
+
+      </tbody>
+    </table>
+  </div>
+
+  <div className="rounded-b-lg border-t border-gray-200 px-4 py-2">
+    <ol className="flex justify-end gap-1 text-xs font-medium">
+      <li>
+        <a
+          href="#"
+          className="inline-flex h-8 w-8 items-center justify-center rounded border border-gray-100 bg-white text-gray-900 rtl:rotate-180"
+        >
+          <span className="sr-only">Prev Page</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-3 w-3"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </a>
+      </li>
+
+      <li>
+        <a
+          href="#"
+          className="block h-8 w-8 rounded border border-gray-100 bg-white text-center leading-8 text-gray-900"
+        >
+          1
+        </a>
+      </li>
+
+      <li
+        className="block h-8 w-8 rounded border-blue-600 bg-blue-600 text-center leading-8 text-white"
+      >
+        2
+      </li>
+
+      <li>
+        <a
+          href="#"
+          className="block h-8 w-8 rounded border border-gray-100 bg-white text-center leading-8 text-gray-900"
+        >
+          3
+        </a>
+      </li>
+
+      <li>
+        <a
+          href="#"
+          className="block h-8 w-8 rounded border border-gray-100 bg-white text-center leading-8 text-gray-900"
+        >
+          4
+        </a>
+      </li>
+
+      <li>
+        <a
+          href="#"
+          className="inline-flex h-8 w-8 items-center justify-center rounded border border-gray-100 bg-white text-gray-900 rtl:rotate-180"
+        >
+          <span className="sr-only">Next Page</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-3 w-3"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </a>
+      </li>
+    </ol>
+  </div>
+</div>
+
+        </Container>
 </>
     );
 };
--- src/Pages/MainlayoutPages/AvalibleCamps.jsx
+++ src/Pages/MainlayoutPages/AvalibleCamps.jsx
@@ -2,8 +2,28 @@ import React from 'react';
 import Container from '../../Shared/Container';
 import bg from '../../assets/avalible.jpg'
 import { Helmet } from 'react-helmet';
+import useAuth from '../../Hook/useAuth';
+import { useQuery } from '@tanstack/react-query';
+import axoissecure from '../../Hook/Axoissecure';
 
 const AvalibleCamps = () => {
+
+  const { user, loading } = useAuth()
+
+   console.log(user?.email)
+  const { data, isLoading } = useQuery({
+    queryKey: ['campinfo'],
+    enabled: !loading && !!user?.email,
+    queryFn: async () => await axoissecure.get('/all-camp'),
+    
+  
+  }
+  
+  
+  )
+
+  console.log(data?.data)
+
     return (
         <>
 
@@ -14,7 +34,7 @@ const AvalibleCamps = () => {
     </Helmet>
 
 
-        <div className='bg-black bg-blend-overlay bg-opacity-70 lg:h-[800px] my-16 bg-fixed lg:mb-40' style={{ backgroundImage: `url('${bg}')`, backgroundPosition: 'center', backgroundSize: 'cover' }}>
+        <div className='bg-black bg-blend-overlay bg-opacity-70 lg:h-[800px] my-16 bg-fixed lg:mb-10' style={{ backgroundImage: `url('${bg}')`, backgroundPosition: 'center', backgroundSize: 'cover' }}>
 
       
 <div className='text-center lg:pt-44 pt-20 md:pt-20'>
@@ -62,9 +82,144 @@ const AvalibleCamps = () => {
 
 </div>
         <Container>
+          
+          <div className='grid lg:grid-cols-3 gap-4 mb-10 grid-cols-1'>
+          {
+            data?.data?.map(item => 
+              
+              
+              <div key={item?._id} >
 
-        
+         <a href="#" className="block rounded-lg p-4 shadow-sm shadow-indigo-100">
+         <img
+           alt="Home"
+           src={item?.image}
+           className="h-56 w-full rounded-md object-cover"
+         />
+       
+         <div className="mt-2">
+           <dl>
+             <div>
+               <dt className="sr-only">Price</dt>
+       
+               <dd className="text-sm text-gray-500">$240,000</dd>
+             </div>
+       
+             <div>
+               <dt className="sr-only">Address</dt>
+       
+               <dd className="font-medium">123 Wallaby Avenue, Park Road</dd>
+             </div>
+           </dl>
+       
+           <div className="mt-6 flex items-center gap-8 text-xs">
+             <div className="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
+               <svg
+                 className="h-4 w-4 text-indigo-700"
+                 xmlns="http://www.w3.org/2000/svg"
+                 fill="none"
+                 viewBox="0 0 24 24"
+                 stroke="currentColor"
+               >
+                 <path
+                   strokeLinecap="round"
+                   strokeLinejoin="round"
+                   strokeWidth="2"
+                   d="M8 14v3m4-3v3m4-3v3M3 21h18M3 10h18M3 7l9-4 9 4M4 10h16v11H4V10z"
+                 />
+               </svg>
+       
+               <div className="mt-1.5 sm:mt-0">
+                 <p className="text-gray-500">Parking</p>
+       
+                 <p className="font-medium">2 spaces</p>
+               </div>
+             </div>
+       
+             <div className="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
+               <svg
+                 className="h-4 w-4 text-indigo-700"
+                 xmlns="http://www.w3.org/2000/svg"
+                 fill="none"
+                 viewBox="0 0 24 24"
+                 stroke="currentColor"
+               >
+                 <path
+                   strokeLinecap="round"
+                   strokeLinejoin="round"
+                   strokeWidth="2"
+                   d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"
+                 />
+               </svg>
+       
+               <div className="mt-1.5 sm:mt-0">
+                 <p className="text-gray-500">Bathroom</p>
+       
+                 <p className="font-medium">2 rooms</p>
+               </div>
+             </div>
+       
+             <div className="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
+               <svg
+                 className="h-4 w-4 text-indigo-700"
+                 xmlns="http://www.w3.org/2000/svg"
+                 fill="none"
+                 viewBox="0 0 24 24"
+                 stroke="currentColor"
+               >
+                 <path
+                   strokeLinecap="round"
+                   strokeLinejoin="round"
+                   strokeWidth="2"
+                   d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                 />
+               </svg>
+       
+               <div className="mt-1.5 sm:mt-0">
+                 <p className="text-gray-500">Bedroom</p>
+       
+                 <p className="font-medium">4 rooms</p>
+               </div>
+               <a
+  className="group  relative inline-flex items-center overflow-hidden rounded border border-current px-8 py-3 text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
+  href="/download"
+>
+  <span className="absolute -end-full transition-all  group-hover:end-4">
+    <svg
+      className="h-5 w-5 rtl:rotate-180"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M17 8l4 4m0 0l-4 4m4-4H3"
+      />
+    </svg>
+  </span>
 
+  <span className="text-sm   font-medium transition-all group-hover:me-4">
+    Join
+  </span>
+</a>
+             </div>
+           </div>
+         </div>
+       </a>
+       
+
+
+              </div>
+              
+              
+              
+              
+              )
+          }
+</div>
         </Container>
         </>
     );
